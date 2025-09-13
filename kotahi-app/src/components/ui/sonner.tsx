1380dfb8ec5ff6763218207eb3583ae33baae261
khosbilegt <archerdoc13@gmail.com>
2025-09-14 00:04:26 +1000

    added sonner for errors

---
+++ kotahi-app/src/components/ui/sonner.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, ToasterProps } from "sonner";
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme();
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group [&_[data-type='error']]:bg-red-500 [&_[data-type='error']]:text-white [&_[data-type='error']_.sonner-icon]:text-red-100"
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+          "--error-bg": "hsl(var(--destructive))",
+          "--error-text": "hsl(var(--destructive-foreground))",
+          "--error-border": "hsl(var(--destructive))",
+        } as React.CSSProperties
+      }
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
